d3a3fea128168c9aa4463517bcb5444c4b6536d0
MithraKM23 <mithrakm2005@gmail.com>
2026-03-03 15:52:11 +0530

    Displaying a welcome message initially

---
+++ UCOnePalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+/*
+ * @author Developer
+ * @version 1.0
+ */
+public class UCOnePalindromeCheckerApp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to palindrome checker Management System");
+		System.out.println("Version: 1.0");
+		System.out.println("System initialized successfully");
+	}
+}
