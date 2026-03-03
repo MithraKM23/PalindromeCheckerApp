b14d84a01847e24bac01ffd574b206147ee27602
MithraKM23 <mithrakm2005@gmail.com>
2026-03-03 16:05:22 +0530

    Printing a hardcoded palindrome result

---
+++ UCTwoPalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+/*
+ * @author Developer
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class UCTwoPalindromeCheckerApp {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Input text: ");
+		String input=sc.nextLine();
+		boolean result=true;
+		for(int i=0;i<input.length()/2;i++) {
+			if(input.charAt(i)!=input.charAt(input.length()-1-i)) {
+				result=false;
+			}
+		}
+		if(result) {
+			System.out.println("Is it a palindrome? : "+result);
+		}
+		else {
+			System.out.println("Is it a palindrome? : "+result);
+		}
+	}
+}
