8d1b34c3ac57e5624e2d436e1d4ff160eaaa2f8f
MithraKM23 <143013805+MithraKM23@users.noreply.github.com>
2026-03-03 16:56:42 +0530

    Checking whether a hardcoded string is a Palindrome

--- UCTwoPalindromeCheckerApp.java
+++ UCTwoPalindromeCheckerApp.java
@@ -8,9 +8,13 @@ import java.util.Scanner;
 public class UCTwoPalindromeCheckerApp {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+
+		//Getting input string 
 		System.out.println("Input text: ");
 		String input=sc.nextLine();
 		boolean result=true;
+
+		//Loop only till half of the string length
 		for(int i=0;i<input.length()/2;i++) {
 			if(input.charAt(i)!=input.charAt(input.length()-1-i)) {
 				result=false;
