6886e1fe4304c21baf25711e8c2e1c4c1b676d8d
MithraKM23 <143013805+MithraKM23@users.noreply.github.com>
2026-03-03 16:59:41 +0530

    Checking a string is a palindrome using string reverse

--- UCThreePalindromeCheckerApp.java
+++ UCThreePalindromeCheckerApp.java
@@ -8,12 +8,18 @@ import java.util.Scanner;
 public class UCThreePalindromeCheckerApp {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+
+		//Getting input String
 		System.out.print("Original: ");
 		String original=sc.nextLine();
+
+		//Using String Builder to reverse the string and storing as a String
 		StringBuilder sb=new StringBuilder(original);
 		String reversed=sb.reverse().toString();
 		System.out.println("Reversed: "+reversed);
 		boolean result=true;
+
+		//if the reversed and original string are same print true
 		if(original.equals(reversed)) {
 			System.out.println("Is Palindrome? "+result);
 		}
